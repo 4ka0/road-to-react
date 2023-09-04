5f2cc23b2cf5ad6ddc8d0156b106f12d1f65698c
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-09-04 20:10:26 +0900

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
 
-function App() {
+const date = {
+  year: "2023",
+  month: "September",
+  day: "4",
+};
 
-  const [button1Count, setButton1Count] = useState(0)
-  const [button2Count, setButton2Count] = useState(0)
-
-  return (
-    <>
-
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Hello, Vite/React template.</h1>
-
-      <div className="card">
-
-        <button onClick={() => setButton1Count((button1Count) => button1Count + 1)}>
-          Increment by 1 = {button1Count}
-        </button>
-
-        <button onClick={() => setButton2Count((button2Count) => button2Count + 2)}>
-          Increment by 2 = {button2Count}
-        </button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR.
-        </p>
-
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more.
-      </p>
-
-    </>
-  )
+function getGreeting(name) {
+  return "Hi, " + name + ".";
+  return "Hi, {name}.";
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <h1>
+        {getGreeting("Jon")}
+        <br />
+        Today is {date.month} {date.day}, {date.year}.
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
